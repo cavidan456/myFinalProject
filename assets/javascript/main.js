c1aaf54a6c7f5c0c8a1285968d5d644ca1883857
Cavidan Abdullayev <146195294+cavidan456@users.noreply.github.com>
2024-02-01 17:43:56 +0400

    procect hero sec compoleted

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,3 +1,66 @@
 // index.html javascript code 
 
 
+document.addEventListener('DOMContentLoaded', function() {
+    var sayacElementi = document.getElementById('sayac');
+    var sayacDegeri = 1;
+
+    function artirSayac() {
+        sayacElementi.textContent = sayacDegeri;
+        sayacDegeri++;
+
+        if (sayacDegeri > 25) {
+         sayacDegeri = 25
+        }
+    }
+
+    setInterval(artirSayac, 100);
+});
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    var sayacElementi = document.getElementById("nomrem");
+    var sayacDegeri = 6000;
+
+    function artirSayac() {
+        sayacElementi.textContent = sayacDegeri;
+        sayacDegeri += 29
+
+        if (sayacDegeri > 9999) {
+         sayacDegeri = 9999
+        }
+    }
+
+    setInterval(artirSayac, 30);
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    var sayacElementi = document.getElementById("nomrem1");
+    var sayacDegeri = 6000;
+
+    function artirSayac() {
+        sayacElementi.textContent = sayacDegeri;
+        sayacDegeri += 29
+
+        if (sayacDegeri > 9999) {
+         sayacDegeri = 9999
+        }
+    }
+
+    setInterval(artirSayac, 30);
+});
+document.addEventListener('DOMContentLoaded', function() {
+    var sayacElementi = document.getElementById("nomrem2");
+    var sayacDegeri = 6000;
+
+    function artirSayac() {
+        sayacElementi.textContent = sayacDegeri;
+        sayacDegeri += 29
+
+        if (sayacDegeri > 9999) {
+         sayacDegeri = 9999
+        }
+    }
+
+    setInterval(artirSayac, 30);
+});
